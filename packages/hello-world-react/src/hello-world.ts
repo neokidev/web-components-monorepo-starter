e8de2ebaa22f906c33b19b6cf5f04ede400e63a0
neoki <ot07neoki@gmail.com>
2024-02-24 23:11:27 +0900

    style: format

--- packages/hello-world-react/src/hello-world.ts
+++ packages/hello-world-react/src/hello-world.ts
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 import { HelloWorld as HelloWorldWC } from '@web-components-monorepo-starter/hello-world'
-import { createComponent } from '@lit/react';
+import { createComponent } from '@lit/react'
 
 export const HelloWorld = createComponent({
   react: React,
   tagName: 'hello-world',
   elementClass: HelloWorldWC,
   displayName: 'HellWorld',
-});
+})
