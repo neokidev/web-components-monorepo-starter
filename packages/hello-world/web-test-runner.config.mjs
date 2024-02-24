c3180270703669a37ee414f2e30dbf3e9e388101
neoki <ot07neoki@gmail.com>
2024-02-24 23:30:20 +0900

    refactor: remove jsdoc @type

--- packages/hello-world/web-test-runner.config.mjs
+++ packages/hello-world/web-test-runner.config.mjs
@@ -1,4 +1,3 @@
 import baseConfig from '@web-components-monorepo-starter/base-configs/web-test-runner.config.mjs'
 
-/** @type {import('@web/test-runner').TestRunnerConfig} */
 export default baseConfig
