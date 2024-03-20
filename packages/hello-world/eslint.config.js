dca3db936bd1b992dee5d85c1c24ca0da091fe9e
neoki <ot07neoki@gmail.com>
2024-03-20 23:55:38 +0900

    chore: update config files

--- packages/hello-world/eslint.config.js
+++
@@ -1,3 +0,0 @@
-import baseConfig from '@web-components-monorepo-starter/base-configs/eslint.base.config.mjs'
-
-export default baseConfig
--- packages/hello-world/vitest.config.mjs
+++
@@ -1,3 +0,0 @@
-import baseConfig from '@web-components-monorepo-starter/base-configs/vitest.base.config.mjs'
-
-export default baseConfig
